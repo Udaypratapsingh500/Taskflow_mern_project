f5c28539402e64195960597d073004f8d6446cf9
harsh <harsh@example.com>
2026-05-24 12:40:57 +0530

    Fixed Render Deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-// server/server.js
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -10,10 +8,11 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
+// Middleware
 app.use(cors());
-
 app.use(express.json());
 
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -23,16 +22,19 @@ mongoose
     console.log(error);
   });
 
+// Routes
 app.use("/api/auth", authRoutes);
-
 app.use("/api/tasks", taskRoutes);
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `Server running on port ${process.env.PORT}`
-  );
+// Port
+const PORT = process.env.PORT || 5000;
+
+// Server Start
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
